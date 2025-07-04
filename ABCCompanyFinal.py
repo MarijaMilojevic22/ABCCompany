9889c43d237b7943a67b11de89bac07932954ce1
Марија Милојевић <ms221061@alas.matf.bg.ac.rs>
2025-07-04 17:57:49 +0200

    Update ABCCompanyFinal.py

--- ABCCompanyFinal.py
+++ ABCCompanyFinal.py
@@ -77,7 +77,7 @@ df_for_impute = pd.concat([
     df[cols_to_impute].reset_index(drop=True),
     pd.DataFrame(df_cat_encoded, columns=cat_cols)
 ], axis=1)
-df_for_impute = df_for_impute.replace({pd.NA: np.nan}).infer_objects()
+df_for_impute = df_for_impute.replace({pd.NA: np.nan}).infer_objects(copy=False)
 
 # Create and apply KNN imputer
 imputer = KNNImputer(n_neighbors=3)
